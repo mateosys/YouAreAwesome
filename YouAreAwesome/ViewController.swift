cad1df1fdd0c23bea52ae5e45f63850ca6a1ee6f
Matthew Sustaita <matthewaccnts@outlook.com>
2022-11-02 20:09:34 -0500

    Completed 2.9

--- YouAreAwesome/ViewController.swift
+++ YouAreAwesome/ViewController.swift
@@ -10,17 +10,46 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-   
+    
+    var imagenumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
-     
+        
     }
+    
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        print(imagenumber)
+        //let imageName = "image" + String(imagenumber)
+        let imageName = "image\(imagenumber)"
 
-
-    @IBAction func messageButtonPressed(_ sender: UIButton) {print("😎 the message button was pressed.")
-        messageLabel.text = "you are awesome!"
-        imageView.image = UIImage(named: "image0")
+        imageView.image = UIImage(named: imageName)
+        imagenumber = imagenumber + 1
+        if imagenumber > 9  {
+            imagenumber = 0
+        }
+        
+        
+        
+        
+        //    let awesomeMessage = "you are Awesome!"
+        //    let greatMessage = " you are Great!"
+        //    let bombMessage = "you are da Bomb!"
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text =  greatMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        } else if messageLabel.text == greatMessage {
+        //            messageLabel.text = bombMessage
+        //            imageView.image = UIImage(named: "image2")
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        
+        //        }
+        
     }
+    
 }
-
